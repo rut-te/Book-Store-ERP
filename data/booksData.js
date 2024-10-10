8fbd684e5af96ebb9e6c73c25071f226a1de2702
rut-te <rutite261@gmail.com>
2024-10-10 09:45:47 +0300

    Add the initial html code

---
+++ data/booksData.js
@@ -0,0 +1,5 @@
+let booksArr=[
+    {id:1,title:'aaa',price:'4',actione:'read',rate:'3',imgUrl:''},
+    {id:2,title:'bbb',price:'8',actione:'read',rate:'1',imgUrl:''},
+    {id:3,title:'aba',price:'7',actione:'read',rate:'7',imgUrl:''}
+];
